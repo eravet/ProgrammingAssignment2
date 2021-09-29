42b3e55f327bca759789b87e7c18da4db427f36d
Eric Ravet <eric.d.ravet@gmail.com>
2021-09-28 22:04:09 -0500

    implemented 2 functions: 1. makeCacheMatrix to accept matrix arg and declare getter/setter functions 2. cacheSolve which calculates the matrix inverse and stores the value in cache

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## makeCacheMatrix creates an object to be passed into cacheSolve(), containing
+## a list of getter/setter functions and stored variables 'i' and 'x'
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        i <- NULL                          # no inverse calculated yet
+        set <- function(y) {               # y must be of type matrix()
+                x <<- y                    # a new matrix is active, and
+                i <<- NULL                 # the inverse hasn't been calculated
+                }
+        get <- function() x                # returns the active matrix, 'x'
+        setinv <- function(inv) i <<- inv  # value passed from cacheSolve
+        getinv <- function() i             # returns inverse value
+        list(set = set,                    # list of functions passed to
+             get = get,                    # cacheSolve() to maintain link
+             getinv = getinv,
+             setinv = setinv)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve() returns the inverse of 'x' from memory if it has already been
+## calculated, or calculates the inverse and stores it to 'i'.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        i <- x$getinv()                           # initialize 'i' from arg func
+        if (!is.null(i)) {                        # return inverse if available
+                message("getting cached inverse")
+                return(i)
+        }
+        data <- x$get()         # if 'i' hasn't been calculated, get data and
+        i <- solve(data, ...)   # calculate the inverse of the passed matrix.
+        x$setinv(i)             # set 'i' to calculated
+        i                       # return calculated inverse value
 }
